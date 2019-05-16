eeec6e44c6ecb46ef399d73a5a757c7a3529df1f
Italo <italo_alves2222@hotmail.com>
2019-05-16 00:57:14 -0300

    Commit Inicial - Tela Home Concluída

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Platform, StatusBar, StyleSheet, View} from 'react-native';
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default class App extends React.Component {
     state = {
@@ -9,7 +10,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <HomeScreen/>
+            <MapScreen/>
         );
     }
 }
