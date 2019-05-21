6b095621dcfd94f203c1fa241125f160bd926833
Rafael J. Nascimento <rafaeljosev10@gmail.com>
2019-05-21 03:49:31 -0300

    Tela de Agenda

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Platform, StatusBar, StyleSheet, View} from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+import Programming from './screens/ProgrammingScreen';
 
 export default class App extends React.Component {
     state = {
@@ -10,7 +11,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <MapScreen/>
+            <Programming/>
         );
     }
 }
