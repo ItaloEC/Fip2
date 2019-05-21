cb6fc72b867858e0f5e1d666c759d6af132e315a
Willian Alves <willianwithi@hotmail.com>
2019-05-21 18:08:34 -0300

    Tela Desafio dos Matões

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {Platform, StatusBar, StyleSheet, View} from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+import ChallengeScreen from './screens/ChallengeScreen';
 
 export default class App extends React.Component {
+    
     state = {
         isLoadingComplete: false,
     };
 
+    // Loading custom fonts of native base cross-platform UI components for React Native
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+          'Roboto': require('native-base/Fonts/Roboto.ttf'),
+          'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+    }
+
     render() {
         return (
-            <MapScreen/>
+            <ChallengeScreen/>
         );
     }
 }
