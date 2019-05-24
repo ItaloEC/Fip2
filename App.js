4b737e0b8b4179c8aff74f3626fc55e998576d88
Willian Alves <willianwithi@hotmail.com>
2019-05-24 16:20:46 -0300

    Alteração da tela desafio de matões para formato de card.

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Platform, StatusBar, StyleSheet, View} from 'react-native';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
 import Information from './screens/InformationScreen';
 import Services from './screens/ServicesScreen';
+import Challenge from './screens/challengeTeste';
 
 
 export default class App extends React.Component {
-    
+
     state = {
         isLoadingComplete: false,
     };
@@ -15,14 +16,14 @@ export default class App extends React.Component {
     // Loading custom fonts of native base cross-platform UI components for React Native
     async componentWillMount() {
         await Expo.Font.loadAsync({
-          'Roboto': require('native-base/Fonts/Roboto.ttf'),
-          'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
         });
     }
 
     render() {
         return (
-            <Services/>
+            <Challenge />
         );
     }
 }
