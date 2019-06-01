bbdd5555c389f71fffb10798593da66457dc24c3
Rafael J. Nascimento <rafaeljosev10@gmail.com>
2019-06-01 16:15:15 -0300

    Permissão para Notificação, e notificação de boas vindas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,9 @@
 import React, {Component} from "react";
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    Animated,
-    Image,
-    ImageBackground,
-    Dimensions,
-    TouchableOpacity,
+import { AppState, StyleSheet, Text, View, ScrollView,  Image, ImageBackground,  TouchableOpacity,
 } from "react-native";
-
-import {Ionicons} from '@expo/vector-icons';
+import { Notifications, Permissions } from "expo";
 
 import {width, height} from '../constants/Layout'
-
 import background from '../assets/backgroud.png';
 import programacao from '../assets/icons/taage_icon_programacao.png';
 import mapa from '../assets/icons/taage_icon_mapa.png';
@@ -26,7 +14,37 @@ import info from '../assets/icons/taage_icon_informacoes.png';
 import noticias from '../assets/icons/taage_icon_noticias.png';
 import desafio from '../assets/icons/taage_icon_desafioserradosmatores.png';
 
+
 export default class HomeScreen extends Component {
+
+    state = {
+        token: "",
+        data: null,
+        origin: null
+    };
+
+    askPermissions = async () => {
+        const { status: existingStatus } = await Permissions.getAsync(
+            Permissions.NOTIFICATIONS
+        );
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+            return false;
+        }
+        return true;
+    };
+
+    sendNotificationImmediately = async () => {
+        let notificationId = await Notifications.presentLocalNotificationAsync({
+            title: "Teste de Notificação",
+            body: "Bem vindo ao FIP2"
+        });
+    };
+
     constructor(props) {
         super(props);
     }
@@ -37,13 +55,11 @@ export default class HomeScreen extends Component {
 
 
     render(props) {
+    this.askPermissions();
+    this.sendNotificationImmediately();
         return (
 
-            <ImageBackground
-                style={styles.container}
-                source={background}
-            >
-
+            <ImageBackground style={styles.container} source={background}>
                     <View style={styles.viewDeCima}></View>
                     <View style={styles.segundaView}></View>
 
@@ -52,94 +68,46 @@ export default class HomeScreen extends Component {
                         <TouchableOpacity
                             style={styles.botoesRole}
                             onPress={() => this.props.navigation.navigate('Programming')}>
-                            <Image
-                                source={programacao}
-                                style={styles.icons}
-                            ></Image>
+                            <Image source={programacao} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Programação</Text>
                         </TouchableOpacity>
-
                         <TouchableOpacity
                             style={styles.botoesRole}
                             onPress={() => this.props.navigation.navigate("Links")}>
-                            <Image
-                                source={mapa}
-                                style={styles.icons}
-                            ></Image>
+                            <Image source={mapa} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Mapa do FIP2</Text>
                         </TouchableOpacity>
-
                     </View>
-
                     <View style={styles.containerBotoes}>
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate("Links")}>
-                            <Image
-                                source={ondecomer}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate("Links")}>
+                            <Image source={ondecomer} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Onde Comer</Text>
                         </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate("Links")}>
-                            <Image
-                                source={ondeficar}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate("Links")}>
+                            <Image source={ondeficar} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Onde Ficar</Text>
                         </TouchableOpacity>
-
                     </View>
-
                     <View style={styles.containerBotoes}>
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate('Services')}>
-                            <Image
-                                source={servicos}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate('Services')}>
+                            <Image source={servicos} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Serviços</Text>
                         </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate('Information')}>
-                            <Image
-                                source={info}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate('Information')}>
+                            <Image source={info} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Informações</Text>
                         </TouchableOpacity>
-
                     </View>
-
                     <View style={styles.containerBotoes}>
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate("Links")}>
-                            <Image
-                                source={noticias}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate("Links")}>
+                            <Image source={noticias} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Notícias</Text>
                         </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.botoesRole}
-                            onPress={() => this.props.navigation.navigate('Challenge')}>
-                            <Image
-                                source={desafio}
-                                style={styles.icons}
-                            ></Image>
+                        <TouchableOpacity style={styles.botoesRole} onPress={() => this.props.navigation.navigate('Challenge')}>
+                            <Image source={desafio} style={styles.icons}></Image>
                             <Text style={styles.textoBotao}>Desafio Serra dos Matoes</Text>
                         </TouchableOpacity>
-
                     </View>
-
                 </ScrollView>
             </ImageBackground>
         );
